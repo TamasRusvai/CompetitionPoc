44eea11bb2b3ffab1423235e10c1e1450c998382
Rusvai Tamas <gofa.spam@gmail.com>
2018-01-01 18:20:38 +0100

    Add MSSQL, EntityFramework and init db with players.

---
+++ CompetitionOrganizerPOC4/Data/CompetitionContext.cs
@@ -0,0 +1,19 @@
+﻿using CompetitionOrganizerPOC4.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompetitionOrganizerPOC4.Data
+{
+    public class CompetitionContext : DbContext
+    {
+        public CompetitionContext(DbContextOptions<CompetitionContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Player> Players { get; set; }
+
+    }
+}
---
+++ CompetitionOrganizerPOC4/Data/DbInitializer.cs
@@ -0,0 +1,38 @@
+﻿using CompetitionOrganizerPOC4.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompetitionOrganizerPOC4.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(CompetitionContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any students.
+            if (context.Players.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            var players = new Player[]
+            {
+            new Player{ Name = "Rusvai Tamás"},
+            new Player{Name = "Cselleng Angéla"},
+            new Player{ Name = "Rusvai Péter"},
+            new Player{ Name = "Halmi József"},
+            new Player{ Name = "Csécs Noémi"}
+            };
+
+            foreach (Player s in players)
+            {
+                context.Players.Add(s);
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
